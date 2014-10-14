b5e826cd0c2bb6e5fd63b17846984e50cddc9f1c
Jason Antman <jason@jasonantman.com>
2014-10-14 14:31:20 -0400

    fix up logging and some other things in simple_example.py

--- examples/simple_action.py
+++ examples/simple_action.py
@@ -1,13 +1,19 @@
 # coding: utf-8
 import logging
+import pprint
 
 from pymco import config
 from pymco import rpc
 from pymco import message
 
-logging.basicConfig()
+FORMAT = "[%(levelname)s %(filename)s:%(lineno)s - %(name)s.%(funcName)s() ] %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+logger = logging.getLogger(__name__)
 
 config = config.Config.from_configfile('client.cfg')
 msg = message.Message(body='ping', agent='discovery', config=config)
+logger.debug("calling rpc.SimpleAction")
 action = rpc.SimpleAction(config=config, msg=msg, agent='discovery')
-print action.call()
+logger.debug("SimpleAction call finished")
+results = action.call()
+pprint.pprint(results)
